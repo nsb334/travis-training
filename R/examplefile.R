eb565b0bfd7d90bdc13bfcae09b2b3a834f9732d
Nikolas Burkoff <Nikolas.Burkoff@Tessella.com>
2016-02-16 15:03:08 +0000

    master: Add coxph wrapper and description file

--- R/examplefile.R
+++ R/examplefile.R
@@ -1,7 +1,19 @@
+##' @importFrom survival coxph
+NULL
+
 ##' squares a value x
 ##' @param x The number to be squared
 ##' @return x*x
 ##' @export
 square <- function(x){
-  x*x  
+  x*x
+}
+
+
+##' A simple wrapper around coxph
+##' @param ... Arguments to be passed to copxh
+##' @return A copxh.object
+##' @export
+coxph_wrapper <- function(...){
+  coxph(...)
 }
